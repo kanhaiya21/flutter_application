d3a56e2d35b5fcf491673bcc3bfb1c89f372544d
kanaiya <kanaiyaasharma54@gmail.com>
2022-11-07 13:03:58 +0545

    Message

--- lib/login.dart
+++ lib/login.dart
@@ -214,7 +214,7 @@ class _MyLoginState extends State<MyLogin> {
                         onTap: (() => Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => forgotpass()))),
+                                builder: (context) => registration()))),
                         child: Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 30.0),
                           child: Row(
